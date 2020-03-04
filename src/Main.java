8b47b19a6ed778654bfdcf26772f1a1cccc189db
shaowei <1175233197@qq.com>
2020-03-04 15:14:58 +0800

    测试

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("asdsd");
+        System.out.println("坤坤");
     }
 }
