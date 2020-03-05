499f5e1097779a8c971947b74504fefd5c68f36b
shaowei <1175233197@qq.com>
2020-03-05 16:29:05 +0800

    CyclicBarrier

--- src/com/sw/test/thread/ThreadSw3.java
+++ src/com/sw/test/thread/ThreadSw3.java
@@ -4,7 +4,9 @@ import java.util.concurrent.BrokenBarrierException;
 import java.util.concurrent.CyclicBarrier;
 
 /**
- * @description: CyclicBarrier
+ * @description: CyclicBarrier(一种同步机制能够对处理一些算法的线程实现同步 ，
+ *就是一个所有线程必须等待的栅栏 ， 所有 ( 一定数量的)线程等待栅栏CyclicBarrier达成，
+ * 所有线程将释放掉继续运行)
  * @author: shaowei
  * @date: 2020-03-05 16:22
  */
@@ -24,10 +26,8 @@ public class ThreadSw3 {
         };
         CyclicBarrier barrier1 = new CyclicBarrier(2, barrierAction1);
         CyclicBarrier barrier2 = new CyclicBarrier(2, barrierAction2);
-        CyclicBarrierRunnable barrierRunnable1 =
-                new CyclicBarrierRunnable(barrier1, barrier2);
-        CyclicBarrierRunnable barrierRunnable2 =
-                new CyclicBarrierRunnable(barrier1, barrier2);
+        CyclicBarrierRunnable barrierRunnable1 = new CyclicBarrierRunnable(barrier1, barrier2);
+        CyclicBarrierRunnable barrierRunnable2 = new CyclicBarrierRunnable(barrier1, barrier2);
         new Thread(barrierRunnable1).start();
         new Thread(barrierRunnable2).start();
     }
