6f886b8a399e7a3824980f9fdf68a15e5155e97b
panym <526062864@qq.com>
2020-03-05 09:40:24 +0800

    新增输出

--- src/Main.java
+++ src/Main.java
@@ -6,5 +6,6 @@ public class Main {
         System.out.println("坤坤");
         System.out.println("傻逼");
         System.out.println("谁是坤坤");
+        System.out.println("Who is yo daddy");
     }
 }
