54a53bc45bff4c28c427bac97e3277da6d2c68a7
panym <526062864@qq.com>
2020-03-05 13:51:53 +0800

    新的测试类

---
+++ src/Test.java
@@ -0,0 +1,10 @@
+/**
+ * @author panym
+ * @Description: ${todo}
+ * @date 2020/3/513:50
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("222222");
+    }
+}
